9d5dc7dd6b8429aca737c15a8c8a82e14f5b036a
unknown <adamxabulkheir@gmail.com>
2023-04-28 22:04:45 -0400

    fixed port issue to properly run server

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/notes', function(req, res) {
-    res.sendFile(__dirname + '/db/public/notes.html');
+    res.sendFile(__dirname + '/develop/public/notes.html');
 });
 
 app.get('*', function(req, res){
-    res.sendFile(__dirname + '/db/public/index.html');
+    res.sendFile(__dirname + '/develop/public/index.html');
 });
 
-app.listen(3000, function() {
-    console.log('port 3000');
+app.listen(3001, function() {
+    console.log('port 3001');
 });
