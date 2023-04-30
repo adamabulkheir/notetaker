d049228f36c554b816bf17e948fd2fba59896678
unknown <adamxabulkheir@gmail.com>
2023-04-29 20:33:40 -0400

    updated folder structure and made changes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+console.log(__dirname);
+
 app.get('/notes', function(req, res) {
-    res.sendFile(__dirname + '/develop/public/notes.html');
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', function(req, res){
-    res.sendFile(__dirname + '/develop/public/index.html');
-});
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.listen(3001, function() {
-    console.log('port 3001');
-});
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
