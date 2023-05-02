30b6ea7d651978a2f55896df863425d21af46394
unknown <adamxabulkheir@gmail.com>
2023-05-01 20:25:34 -0400

    added additional code to server.js and notesroutes.js to enable readFile

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const fs = require('fs')
+const crypto = require('crypto');
+const notes = require('/Users/Laptop/Documents/UCF/homework/notetaker/db/db.json');  
 
 router.get('/notes',(req, res)=>{
     fs.readFile('db/db.json',function(err, data){
@@ -10,4 +12,38 @@ router.get('/notes',(req, res)=>{
     )
 })
 
+// router.post('/notes',(req, res)=>{
+//     fs.writeFile('db/db.json', function(err, data){
+//         if (err) console.log(err)
+//         console.log(data)
+//         res.json(JSON.parse(data))
+//     })
+// })
+
+router.post('/notes', (req, res) => {
+    // Generate a unique ID
+    const id = crypto.randomBytes(16).toString('hex');
+
+    // Create a new note object
+    const newNote = {
+    id: id,
+    title: req.body.title,
+    text: req.body.text
+    };
+
+    // Add the new note object to the notes array
+    notes.push(newNote);
+
+    // Write the entire notes array to the db.json file
+    fs.writeFile('/db/db.json', JSON.stringify(notes), (err) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send('Error writing note data');
+        return;
+}
+
+    console.log('Note saved successfully');
+    res.send('Note saved successfully');
+    });
+});
 module.exports = router
